b70e49ec87e901b018b0531e729558577ec0f525
IvkinNikita <n.nikita.ivkin@gmail.com>
2025-12-02 22:41:34 +0300

    working main.tf

---
+++ Terraform/infra_stage/provider.tf
@@ -0,0 +1,15 @@
+# Объявление провайдера
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 1.00"
+}
+
+provider "yandex" {
+  zone                     = "ru-central1-a"
+  folder_id                = var.folder_id
+  service_account_key_file = "../sa-key.json"
+}
--- Terraform/main.tf
+++
@@ -1,4 +0,0 @@
-provider "yandex" {
-    zone      = "ru-central1-a"
-    folder_id = var/folder_id
-}
